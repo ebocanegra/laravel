7929b8ae9f914580e352473ada1bd0e04ad1db23
ebocanegra <bocanegraedu0@gmail.com>
2020-04-16 20:04:51 +0200

    Arreglar el problema de los controlers y avanzar hasta el control CSRF en la API

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+// Hacemos uso del modelo User.
+use App\User;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -15,8 +18,14 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
 		$this->call('FabricanteSeeder');
-	   	$this->call('AvionSeeder');
-		//$this->call('UserTableSeeder');
+		$this->call('AvionSeeder');
+
+		// Solo queremos un único usuario en la tabla, así que truncamos primero la tabla
+		// Para luego rellenarla con los registros.
+		User::truncate();
+
+		// LLamamos al seeder de Users.
+		$this->call('UserSeeder');
 	}
 
 }
